6abf56b2eebbf3ee715e186ec08cda19cbdc0321
Rosa0611 <rosero.rosa@correounivalle.edu.co>
2025-01-28 14:14:25 -0500

    update controller
    se agrega conflictException, NotFoundException, HttpCode

--- backendapi/src/products/products.controller.ts
+++ backendapi/src/products/products.controller.ts
@@ -2,6 +2,9 @@ import { Controller, Get, Post, Delete, Put, Body, Param } from '@nestjs/common'
 import { productsService } from './products.service';
 import { createProductsDto } from 'src/dto/create-products-dto';
 import { updateProductsDto } from 'src/dto/update-products-dto';
+import { ConflictException } from '@nestjs/common';
+import {NotFoundException} from '@nestjs/common';
+import { HttpCode } from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
@@ -9,9 +12,17 @@ export class ProductsController {
     }
 
     @Post('/')
-    create(@Body() body:createProductsDto) {
-        return this.productsService.create(body);
+    async create(@Body() body:createProductsDto){
+        try {
+            return await this.productsService.create(body);  
+        } catch (error) {
+            if (error.code === 11000){
+                throw new ConflictException('El producto ya esta Registrado');
+            }
+            throw error;
+        }
     }
+
 
     @Get()
     findAll() {
@@ -19,17 +30,46 @@ export class ProductsController {
     }
 
     @Get(':id')
-    findOne(@Param('id') id:string) {
-        return this.productsService.findOne(id);
+    async findOne(@Param('id') id:string){
+        try {
+            const act = await this.productsService.findOne(id);
+            if (!act ){
+                throw new NotFoundException('El producto no existe');
+            } 
+            return act;
+        } catch (error) {
+            throw error;
+        }
     }
+
 
     @Put(':id')
-    update(@Param('id') id:string, @Body() body: updateProductsDto) {
-        return this.productsService.update(id,body);
+    async update(@Param('id') id:string, @Body() body: updateProductsDto){
+        try { 
+            const act = this.productsService.update(id,body);
+            if(!act){
+                throw new NotFoundException('La Actividad no Existe');
+            }
+            return act;
+        } catch (error) {
+            throw error;
+        }      
     }
 
+
     @Delete(':id')
-    delete(@Param('id') id:string) {
-        this.productsService.delete(id);	
+
+    @HttpCode(204)
+    async delete(@Param('id') id:string){
+        try {
+            const act = this.productsService.delete(id);
+            if (!act ){
+                throw new NotFoundException('El producto no existe');
+            } 
+            return act;
+        } catch (error) {
+            throw error;
+        }
     }
+
 }
