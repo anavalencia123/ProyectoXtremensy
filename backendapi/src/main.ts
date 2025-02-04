bfd14666f15e3409414f3017f5c4f8a19d5c20df
charliertt <carlitosviveros588@gmail.com>
2025-02-04 10:58:25 -0500

    lista de productos

--- backendapi/src/main.ts
+++ backendapi/src/main.ts
@@ -5,7 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3050);
 }
 bootstrap();
+
